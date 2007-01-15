ef4d77929ea0516f621af7c20508fc73743e2e6c
Sylvain Beucler <beuc@beuc.net>
2007-01-15 19:03:42 +0000

    demo_feed: generate names for table 'destinataire'
    git-archimport-id: devel@patapouf.org--patapouf-arch/gcourrier--mainline--1.0--patch-105

--- demo_feed.php
+++ demo_feed.php
@@ -4,13 +4,17 @@ setlocale(LC_ALL, 'fr_FR.UTF-8');
 header('Content-type: text/plain');
 require_once "Text/Password.php";
 
+function gen_nom() {
+  $nom  = Text_Password::create();
+  $nom{0} = strtoupper($nom{0});
+  return $nom;
+}
+
 $prenoms = array("Blandine", "Claudine", "Edith", "Hervé", "Jean-François", "Jean-Luc", "Jean-Paul", "Liliane", "Martine", "Nathalie", "Patricia", "Stéphanie", "Anne-marie", "Bernadette", "Christine", "David", "Francelina", "Georges", "Gérard", "Grégory", "Carine", "Marcel", "Marianne", "Sylvie", "Willy", "Yves");
 
 $noms = array();
 foreach($prenoms as $prenom) {
-  $nom  = Text_Password::create();
-  $nom{0} = strtoupper($nom{0});
-  $noms[] = $nom;
+  $noms[] = gen_nom();
   $service = rand(2, 7);
   $login = strtolower($prenom);
   echo "INSERT INTO utilisateur
@@ -29,10 +33,6 @@ $services[] = array('INFO', "Services informatiques");
 foreach ($services as $service) {
   echo "INSERT INTO service (libelle, designation) VALUES('{$service[0]}', '{$service[1]}');\n";
 }
-
-$rues = array("avenue", "rue", "boulevard", "impasse");
-
-$noms_rues = array("Allard", "Henri Becquerel", "René Cassin", "Raoul Briquet", "Kléber", "de la liberté", "du Tchard", "Therry", "Thiers", "Jean Médecin", "Pasteur", "Alphonse de Lamartine", "Fragonard", "Valrose");
 
 $accuse['expediteur'] = "Mairie de Panel-sur-mer";
 $accuse['adresse'] = "Hôtel de Ville";
@@ -54,12 +54,42 @@ $priorities[1]['designation'] = 'basse';
 $priorities[1]['nbJours'] = 15;
 
 
+
+
 foreach ($priorities as $priority) {
   echo "INSERT INTO priorite (designation, nbJours)
         VALUES('{$priority['designation']}', {$priority['nbJours']});\n";
 }
 
-# fournisseurs
+
+$rues = array("avenue", "rue", "boulevard", "impasse");
+
+$noms_rues = array("Allard", "Henri Becquerel", "René Cassin", "Raoul Briquet",
+		   "Kléber", "de la liberté", "du Tchard", "Therry", "Thiers",
+		   "Jean Médecin", "Pasteur", "Alphonse de Lamartine",
+		   "Fragonard", "Valrose");
+$villes = array("Montigny", "St-Martin", "Ailleurs", "Labas",
+  "Panel-sur-mer", "Mareil-les-bains");
+
+// Fournisseurs
+for ($i = 0; $i < 10; $i++) {
+  $nom = gen_nom();
+  $prenom = $prenoms[rand(0,count($prenoms)-1)];
+  $rue = $rues[rand(0,count($rues)-1)];
+  $nom_rue = $noms_rues[rand(0,count($noms_rues)-1)];
+  $ville = strtoupper($villes[rand(0,count($villes)-1)]);
+  $numero = rand(1,90);
+  $adresse = "$numero $rue $nom_rue";
+  $cp1 = sprintf("%02d", rand(1,95));
+  $cp2 = sprintf("%03d", rand(1,999));
+  $code_postal = "$cp1$cp2";
+  $telephone = sprintf("%02d", rand(1,5));
+  for ($t = 0; $t < 4; $t++)
+    $telephone .= sprintf("%02d", rand(0,99));
+
+  echo "INSERT INTO destinataire (nom, prenom, adresse, codePostal, ville, telephone)
+    VALUES ('$nom', '$prenom', '$adresse', '$code_postal', '$ville', '$telephone');\n";
+}
 
 # facture
 # courrier entrant
