732b389ca6b97e0cb73062323d19118eac268c93
Sylvain Beucler <beuc@beuc.net>
2007-09-27 19:29:33 +0000

    Forgot a copyright notice in demo_feed.php
    git-archimport-id: devel@patapouf.org--patapouf-arch/gcourrier--mainline--1.0--patch-132

--- demo_feed.php
+++ demo_feed.php
@@ -1,4 +1,24 @@
 <?php
+/*
+Generate random data for the online demo
+Copyright (C) 2007  Cliss XXI
+
+This file is part of GCourrier.
+
+GCourrier is free software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation, either version 3 of the License, or (at your
+option) any later version.
+
+GCourrier is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see http://www.gnu.org/licenses/.
+*/
+
 setlocale(LC_ALL, 'fr_FR.UTF-8');
 #header('Content-type: text/html; charset=UTF-8');
 header('Content-type: text/plain');
