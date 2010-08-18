0aadefb60246dfd3117b5c29f73b0a427918f61d
Sylvain Beucler <beuc@beuc.net>
2010-08-18 22:31:35 +0200

    La 1.6, c'était il y a plus de 4 ans...

--- templates/footer.php
+++ templates/footer.php
@@ -1,7 +1,7 @@
     <div id="bottom_navbar"><a href=".">Index</a></div>
     </div>
     <div id="version">
-      GCourrier 1.6
+      GCourrier 1.7
       - <a href="copyright.html">Licence</a>
     </div>
   </body>
